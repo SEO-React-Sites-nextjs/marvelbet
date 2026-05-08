bece480fbe5e71df9fd98516ce1522e0e168e140
Jeff003 <xeme003@xeme-design.com>
2026-05-08 14:03:33 +0800

    feat: create config for global

---
+++ config.ts
@@ -0,0 +1,11 @@
+
+
+
+// TODO 有機會就把散落在各處的硬編碼替換成變數
+const config = {
+    baseUrl: "https://www.marvel365bet.com",
+    articleAuthor: "marvel365bet",
+    articlePublisher: "marvel365bet"
+}
+
+export { config }
